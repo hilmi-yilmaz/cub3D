d64df4baf03c00e9334545d32fe4a573e63c472b
Hilmi Yilmaz <hilmiyilmaz.1998@gmail.com>
2021-10-12 19:13:15 +0200

    removed redundant variable

--- cub3d.h
+++ cub3d.h
@@ -1,7 +1,6 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
-/* Include headerfiles */
 # include "raycaster/raycaster_structs.h"
 # include "parser/parse_structs.h"
 
@@ -10,35 +9,5 @@
 
 # include "raycaster/utils/raycaster_free.h"
 # include "parser/utils/parser_free.h"
-
-/* If not defined, we are on OSX */
-// # ifndef __linux__
-// #  define __linux__ 0
-// # endif
-
-// /* Define keybindings for linux and macosx */
-// # ifdef __linux__
-// #  include "mlx_linux/mlx.h"
-// #  define UP_KEY 65362
-// #  define DOWN_KEY 65364
-// #  define RIGHT_KEY 65363
-// #  define LEFT_KEY 65361
-// #  define A_KEY 97
-// #  define D_KEY 100
-// #  define W_KEY 119
-// #  define S_KEY 115
-// #  define ESC_KEY 65307
-// # else
-// #  include "mlx/mlx.h"
-// #  define UP_KEY 126
-// #  define DOWN_KEY 125
-// #  define RIGHT_KEY 124
-// #  define LEFT_KEY 123
-// #  define A_KEY 0
-// #  define D_KEY 2
-// #  define W_KEY 13
-// #  define S_KEY 1
-// #  define ESC_KEY 53
-// # endif
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hyilmaz <hyilmaz@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/03/21 16:23:31 by hyilmaz       #+#    #+#                 */
-/*   Updated: 2021/10/12 13:58:50 by hyilmaz       ########   odam.nl         */
+/*   Updated: 2021/10/12 19:12:59 by hyilmaz       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,16 @@
 int	main(int argc, char **argv)
 {
 	t_data	data;
-	int		parse_return;
-	int		raycaster_return;
+	int		ret;
 
-	parse_return = parse_main(&data.parse, argv);
-	if (parse_return == -1)
+	ret = parse_main(&data.parse, argv);
+	if (ret == -1)
 	{
 		free_parse(&data.parse);
 		return (-1);
 	}
-	raycaster_return = raycaster_main(&data);
-	if (raycaster_return == -1)
+	ret = raycaster_main(&data);
+	if (ret == -1)
 	{
 		free_all(&data);
 		return (-1);
