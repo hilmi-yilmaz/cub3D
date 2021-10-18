faa9e5cdaf5c4aa10d2a2acdbdccebc0f084e448
hilmi-yilmaz <hilmiyilmaz.1998@gmail.com>
2021-10-18 15:30:48 +0200

    removed X.h headerfile, only used macros that are needed in raycaster_main.h

--- raycaster/raycaster_main.h
+++ raycaster/raycaster_main.h
@@ -6,7 +6,7 @@
 /*   By: hyilmaz <hyilmaz@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/18 15:11:31 by hyilmaz       #+#    #+#                 */
-/*   Updated: 2021/10/18 15:11:32 by hyilmaz       ########   odam.nl         */
+/*   Updated: 2021/10/18 15:29:38 by hyilmaz       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <stddef.h>
 
 /* User defined headers */
-# include "../incl/X.h"
+//# include "../incl/X.h"
 # include "../incl/is_linux.h"
 # include "raycaster.h"
 # include "gameplay.h"
@@ -28,6 +28,14 @@
 # include "../parser/utils/parser_utils.h"
 
 /* Macros */
+# define KeyPress 2
+# define KeyPressMask (1L<<0)
+# define KeyRelease 3
+# define KeyReleaseMask (1L<<1)
+# define ClientMessage 33
+# define NoEventMask 0L
+# define DestroyNotify 17
+# define StructureNotifyMask (1L<<17)
 # define UNIT 1
 
 /* Function prototypes */
